09f0d210ac893a64f9fc9e303efd120957fc8e0c
Tyler C <tyler@candeegenerations.com>
2018-08-25 09:19:45 -0400

    Setup Project.

---
+++ lint-staged.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  linters: {
+    'src/**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
+      // 'eslint --fix',
+      'prettier --write',
+      'git add',
+    ],
+  },
+}
